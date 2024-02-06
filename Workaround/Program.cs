b1f1acae94496a3bc8e4d1736cc95f7a654d6cb2
alihs <alihs@ALI>
2024-02-06 21:10:53 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.NationalId = 12345678910;
+        person1.FirstName = "ALİ İHSAN ÖZKAYA";
+        person1.LastName = "ÖZKAYA";
+        person1.DateOfBirthYear = 2000;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+    }
+}
